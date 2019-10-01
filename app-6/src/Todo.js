d349e6e897577f841a594f3c9ef474c1507dc142
Shane Gallatin <shane.gallatin@verizon.net>
2019-10-01 16:33:41 -0500

    finished 4-7

---
+++ app-6/src/Todo.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react"
+
+class Todo extends Component{
+    constructor(){
+        super();
+
+        this.state = {
+
+
+        }
+    }
+
+
+    render(){
+        return(
+            <p>{this.props.task}</p>
+
+        );
+    }
+}
+
+export default Todo; 
---
+++ app-7/src/List.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import Todo from "./Todo"
+
+class List extends Component{
+    constructor(){
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        let list = this.props.tasks.map((element, index) => {
+            return <Todo key={index} task={element} />
+        });
+
+        return(
+            <div>{list}</div>
+        )
+    }
+}
+
+export default List
